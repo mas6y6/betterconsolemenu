a2895b7d0d313ac9d689d4f19b778ad559ffe239
mas6y6 <mlopezjr2020@gmail.com>
2024-06-10 21:38:53 -0500

    Update

---
+++ bcm/__init__.py
@@ -0,0 +1,1 @@
+from bcm import *
---
+++ bcm/bcm.py
@@ -0,0 +1,19 @@
+class Menu:
+    def __init__(self):
+        self.items = []
+    
+    def add_item(self,item):
+        self.items.append(item)
+    
+    def remove_item(self,pop):
+        self.items.pop(pop)
+    
+    def _run(self,include_exit):
+        _items = []
+        if include_exit:
+            _items.append("_exit")
+        
+        while
+    
+    def run(self):
+        self._run()
