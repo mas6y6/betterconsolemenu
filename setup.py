464d4fd5eecc7e95b4c21c671d032422e8ccff57
mas6y6 <mlopezjr2020@gmail.com>
2024-06-10 21:11:23 -0500

    Start Upload

---
+++ setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(
+    name="BetterConsoleMenu",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    packages=["bcm"],
+)
