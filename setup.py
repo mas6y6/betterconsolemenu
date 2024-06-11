f10cae4de38c7642557345db67a4eee2d5272e4f
mas6y6 <mlopezjr2020@gmail.com>
2024-06-10 21:15:56 -0500

    Update

--- setup.py
+++ setup.py
@@ -1,8 +1,11 @@
 from setuptools import setup
 
 setup(
-    name="BetterConsoleMenu",
+    name="betterconsolemenu",
     long_description=open("README.md").read(),
     long_description_content_type="text/markdown",
     packages=["bcm"],
+    url="https://github.com/mas6y6/betterconsolemenu",
+    description="Console menu but better and fancy",
+    download_url=https://github.com/mas6y6/betterconsolemenu
 )
